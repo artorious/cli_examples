b8a654b26cee86e21a37337647c6bcd5b45acd1f
Arthur Ngondo <ngondoarthur@gmail.com>
2018-06-15 15:37:13 +0300

    Add commadline arguments - click module
    Adds required positional arguments <name> to run script

--- click_dir/app.py
+++ click_dir/app.py
@@ -1,19 +1,31 @@
+"""A program that says hello or goodbye to provided username."""
 import click
 
 
 @click.group()
 def greet():
+    """ Say hello or goodbye."""
     pass
 
 
 @greet.command()
+@click.argument('name')  # Add name arg
 def hello(**kwargs):
-    pass
+    """ (str) -> stdout
+
+        Say Hello to user <name>
+    """
+    print('Hello, {0}!'.format(kwargs['name']))
 
 
 @greet.command()
+@click.argument('name')
 def goodbye(**kwargs):
-    pass
+    """ (str) -> stdout
+
+        Say Goodbye to user <name>
+    """
+    print('Goodbye {0}!'.format(kwargs['name']))
 
 
 if __name__ == '__main__':
